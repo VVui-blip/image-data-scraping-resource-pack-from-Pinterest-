7c8f2417c278c4fcd7e41d0bbc5f186bd4d5e00f
VVui-blip <vuv54581@gmail.com>
2026-06-21 12:00:22 +0700

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ search_extra = [r for r in all_requirements if r.lower().startswith("ddgs")]
 
 setup(
     name="pingrabber",
-    version="1.0.0",
+    version="1.0.1",
     description="Thư viện cào ảnh chất lượng cao từ board Pinterest công khai qua RSS feed.",
     long_description=long_description,
     long_description_content_type="text/markdown",
